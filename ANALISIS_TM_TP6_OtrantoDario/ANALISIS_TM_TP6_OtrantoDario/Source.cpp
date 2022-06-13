1b753b5159b831d687ca637e1cb93e54679a934c
Dario <otrantodario@gmail.com>
2022-06-13 01:30:55 -0300

    Ship selection system added

--- ANALISIS_TM_TP6_OtrantoDario/ANALISIS_TM_TP6_OtrantoDario/Source.cpp
+++ ANALISIS_TM_TP6_OtrantoDario/ANALISIS_TM_TP6_OtrantoDario/Source.cpp
@@ -10,13 +10,14 @@ void showPrincipalBoard(int turn);
 void showShotBoard(int turn);
 int gameplay(bool newGame);
 bool resetBoards();
+bool setBoardShips();
 
 HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 
-COORD consolecursorposition = {48,1}; // posicion del cursor en pantalla
+COORD consolecursorposition = { 48,1 }; // posicion del cursor en pantalla
 int pointerCursor = 0;
-int totalShipsP1 = 0;
-int totalShipsP2 = 0;
+int totalShipsP1 = 10;
+int totalShipsP2 = 10;
 const int maxBoardSize = 10;
 
 int player1PrincipalBoard[maxBoardSize][maxBoardSize];
@@ -71,6 +72,74 @@ void main()
 	} while (ProgramOn);
 
 
+}
+int gameplay(bool newGame) // funcion que contiene el juego
+{
+	int userAnsw;
+	int backToMainMenu = 0;
+	int plyVsCpu = 1;
+	int plyVsPly = 2;
+	int turn = 0;
+	bool newGameGamplay = newGame;
+	bool fase1Player1 = true;
+	bool fase1Player2 = true;
+	bool continuePlaying = true;
+	do
+	{
+		if (newGame)
+		{
+			newGameGamplay = resetBoards();
+		}
+		system("cls");
+		SetConsoleTextAttribute(h, 11);
+		cout << R"(					 ____  ____  ____  ____  _________  ____  ____  _________  ____  ____  ____  ____  ____ 
+					||M ||||o ||||d ||||o ||||       ||||d ||||e ||||       ||||J ||||u ||||e ||||g ||||o ||
+					||__||||__||||__||||__||||_______||||__||||__||||_______||||__||||__||||__||||__||||__||
+					|/__\||/__\||/__\||/__\||/_______\||/__\||/__\||/_______\||/__\||/__\||/__\||/__\||/__\|)" << endl;
+		SetConsoleTextAttribute(h, 9);
+		cout << endl;
+		if (pointerCursor == plyVsCpu)
+		{
+			SetConsoleTextAttribute(h, 144);
+		}
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t"".---------------." << endl;
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""| Player vs CPU |" << endl;
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""'---------------'" << endl;
+		cout << endl;
+		SetConsoleTextAttribute(h, 9);
+		if (pointerCursor == plyVsPly)
+		{
+			SetConsoleTextAttribute(h, 144);
+		}
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b"".------------------." << endl;
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b""| Player vs Player |" << endl;
+		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b""'------------------'" << endl;
+		cout << endl;
+		SetConsoleTextAttribute(h, 9);
+		userAnsw = pointer(plyVsPly, plyVsCpu);
+		if (userAnsw == plyVsCpu)
+		{
+			system("cls");
+			cout << "Player vs CPU" << endl;
+			continuePlaying = false;
+			system("pause");
+		}
+		else if (userAnsw == plyVsPly)
+		{
+			system("cls");
+			do
+			{
+				//cout << "Player vs Player" << endl;
+				showPrincipalBoard(turn);
+				fase1Player1 =  setBoardShips();
+				system("cls");
+			} while (fase1Player1);
+			
+			system("pause");
+		}
+	} while (continuePlaying);
+	pointerCursor = backToMainMenu;
+	return backToMainMenu;
 }
 int showMenu(int mainMenu) // funcion para mostrar el menu principal
 {
@@ -169,72 +238,6 @@ int pointer(int maxOption, int minOption)
 		break;
 	}
 } // cursor para el menu
-int gameplay(bool newGame) // funcion que contiene el juego
-{
-	int userAnsw;
-	int backToMainMenu = 0;
-	int plyVsCpu = 1;
-	int plyVsPly = 2;
-	int turn = 0;
-	bool newGameGamplay = newGame;
-	bool fase1Player1 = true;
-	bool fase1Player2 = true;
-	bool continuePlaying = true;
-	do
-	{
-		if (newGame)
-		{
-			newGameGamplay = resetBoards();
-		}
-		system("cls");
-		SetConsoleTextAttribute(h, 11);
-		cout << R"(					 ____  ____  ____  ____  _________  ____  ____  _________  ____  ____  ____  ____  ____ 
-					||M ||||o ||||d ||||o ||||       ||||d ||||e ||||       ||||J ||||u ||||e ||||g ||||o ||
-					||__||||__||||__||||__||||_______||||__||||__||||_______||||__||||__||||__||||__||||__||
-					|/__\||/__\||/__\||/__\||/_______\||/__\||/__\||/_______\||/__\||/__\||/__\||/__\||/__\|)" << endl;
-		SetConsoleTextAttribute(h, 9);
-		cout << endl;
-		if (pointerCursor == plyVsCpu)
-		{
-			SetConsoleTextAttribute(h, 144);
-		}
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t"".---------------." << endl;
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""| Player vs CPU |" << endl;
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""'---------------'" << endl;
-		cout << endl;
-		SetConsoleTextAttribute(h, 9);
-		if (pointerCursor == plyVsPly)
-		{
-			SetConsoleTextAttribute(h, 144);
-		}
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b"".------------------." << endl;
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b""| Player vs Player |" << endl;
-		cout << "\t""\t""\t""\t""\t""\t""\t""\t""\t""\t""\b""'------------------'" << endl;
-		cout << endl;
-		SetConsoleTextAttribute(h, 9);
-		userAnsw = pointer(plyVsPly, plyVsCpu);
-		if (userAnsw == plyVsCpu)
-		{
-			system("cls");
-			cout << "Player vs CPU" << endl;
-			continuePlaying = false;
-			system("pause");
-		}
-		else if (userAnsw == plyVsPly)
-		{
-			system("cls");
-			if (fase1Player1)
-			{
-				//cout << "Player vs Player" << endl;
-				showPrincipalBoard(turn);
-				showShotBoard(turn);
-			}
-			system("pause");
-		}
-	} while (continuePlaying);
-	pointerCursor = backToMainMenu;
-	return backToMainMenu;
-}
 bool resetBoards() // funcion para resetear los tableros
 {
 	int defaultValue = 0; // valor default para los tableros
@@ -244,7 +247,7 @@ bool resetBoards() // funcion para resetear los tableros
 	memset(player2ShotBoard, defaultValue, maxBoardSize); // resetea el tablero de tiro del jugador 2
 	return false;
 }
-void showPrincipalBoard(int turn) 
+void showPrincipalBoard(int turn)
 {
 	char uperLeftCorner = 201; // esquina superior izquierda ╔
 	char uperRightCorner = 187; // esquina superior derecha ╗
@@ -262,7 +265,7 @@ void showPrincipalBoard(int turn)
 	char cros = 88;		// valor "X" en el tablero
 	char circle = 79;	// valor "O" en el tablero
 
-
+	SetConsoleTextAttribute(h, 9);
 	cout << "\t" << uperLeftCorner;
 	for (int i = 0; i < maxBoardSize; i++)
 	{
@@ -316,7 +319,7 @@ void showPrincipalBoard(int turn)
 	cout << "\b" << lowerRightCorner;
 	cout << endl;
 }
-void showShotBoard(int turn) 
+void showShotBoard(int turn)
 {
 	char uperLeftCorner = 201; // esquina superior izquierda ╔
 	char uperRightCorner = 187; // esquina superior derecha ╗
@@ -329,7 +332,7 @@ void showShotBoard(int turn)
 	char horizontalConection = 185; // conector derecho ╣
 	char verticalConection = 204; // conector izquierdo ╠
 	char crosConection = 206; // interseccion ╬
-	char squareConection = 219;
+	char squareConection = 219; // conexion entre tableros █
 
 	char empty = 32;	// valor vacio en el tablero
 	char cros = 88;		// valor "X" en el tablero
@@ -397,4 +400,70 @@ void showShotBoard(int turn)
 	}
 	cout << "\b" << lowerRightCorner;
 	cout << endl;
+}
+bool setBoardShips() 
+{
+	enum class Ships {Acorazado = 1,Crucero,Destructor,Submarino};
+
+	cout << "Selecione el barco que quiera usar" << endl;
+	char water = 126;
+	char acorazado = 219;
+	char crucero = 175;
+	char destructor = 223;
+	char submarino = 220;
+	char cros = 88;		// valor "X" en el tablero
+	
+	int shipSelected = 0;
+	int allShipsPlaced = 0;
+
+	//cout << "\t" << water << " = Agua" << endl;
+	//cout << "\t" << cros << " = HIT!" << endl;
+	SetConsoleTextAttribute(h, 11);
+	if (pointerCursor == (int)Ships::Acorazado)
+	{
+		SetConsoleTextAttribute(h, 144);
+	}
+	cout << "\t" << " " << acorazado << " = Acorazado 4 casillas" << endl;
+	SetConsoleTextAttribute(h, 11);
+	if (pointerCursor == (int)Ships::Crucero)
+	{
+		SetConsoleTextAttribute(h, 144);
+	}
+	cout << "\t" << " " << crucero << " = Crucero 3 casillas" << endl;
+	SetConsoleTextAttribute(h, 11);
+	if (pointerCursor == (int)Ships::Destructor)
+	{
+		SetConsoleTextAttribute(h, 144);
+	}
+	cout << "\t" << " " << destructor << " = Destructor 2 casillas" << endl;
+	SetConsoleTextAttribute(h, 11);
+	if (pointerCursor == (int)Ships::Submarino)
+	{
+		SetConsoleTextAttribute(h, 144);
+	}
+	cout << "\t" << " " << submarino << " = Submarino 1 casilla" << endl;
+	SetConsoleTextAttribute(h, 11);
+	shipSelected = pointer((int)Ships::Submarino, (int)Ships::Acorazado);
+	switch (shipSelected)
+	{
+	case (int)Ships::Acorazado:
+		break;
+	case (int)Ships::Crucero:
+		break;
+	case (int)Ships::Destructor:
+		break;
+	case (int)Ships::Submarino:
+		break;
+	default:
+		cout << "Error" << endl;
+		break;
+	}
+	if (totalShipsP1 == allShipsPlaced)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
 }
